99272fcfabde67ced489848c8020eb39fbdcb149
Alba Yerga Castells <ayerga@altech.es>
2024-05-07 14:06:27 +0200

    chore: refactoring router and controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,31 +1,22 @@
-import { AppDataSource } from "../..";
-import { Task } from "./tasks.entity";
-import { instanceToPlain } from "class-transformer";
+import { AppDataSource } from "../.."
+import { Task } from "./tasks.entity"
+import { instanceToPlain } from "class-transformer"
+import { Request, Response } from "express"
 
-export class TasksController {
-
-    constructor(private tasksRepository = AppDataSource.getRepository(
-        Task,
-    )) {}
-
-    // @ts-ignore
-    public async getAll(): Promise<Task[]> {
+class TasksController {
+    public async getAll(req: Request, res: Response): Promise<Response> {
         // Declara a variable to hold all tasks
-        let allTasks: Task[];
-
-        // Fetch all tasks using the reporitory
-        try
-        {
-            allTasks = await this.tasksRepository.find(); // { order: { date: 'ASC', }, } inside find?
-
+        let allTasks: Task[]
+        try {
+            // Fetch all tasks using the reporitory
+            allTasks = await AppDataSource.getRepository(Task).find() // { order: { date: 'ASC', }, } inside find?
             // Convert the tasks instance to an array of objects
-            allTasks = instanceToPlain(allTasks) as Task[];
-
-            return allTasks;
-        }
-        catch(errors)
-        {
-            console.log(errors)
+            allTasks = instanceToPlain(allTasks) as Task[]
+            return res.json(allTasks).status(200)
+        } catch (_errors) {
+            return res.json({ error: "Internal Server Error" }).status(500)
         }
     }
 }
+
+export const tasksController = new TasksController() // exporting the instance of the task
--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,31 +1,22 @@
-import { Router, Response, Request } from "express";
-import { TasksController } from "./tasks.controller";
-import { createValidator } from "./tasks.validator";
-import { validationResult } from "express-validator";
+import { Router, Response, Request } from "express"
+import { tasksController } from "./tasks.controller"
+import { createValidator } from "./tasks.validator"
+import { validationResult } from "express-validator"
 
 // Fire the router function
-export const tasksRouter: Router = Router();
+export const tasksRouter: Router = Router()
 
 // Create default route
-tasksRouter.get(
-    '/tasks',
-    async (req: Request, res: Response)=>{
-        // res.send('Express + TypeScript Server');
-        const tasksController = new TasksController();
-        const allTasks = await tasksController.getAll();
-        res.json(allTasks).status(200);
-    }
-)
+tasksRouter.get("/tasks", tasksController.getAll)
 
 tasksRouter.post(
-    '/tasks',
+    "/tasks",
     createValidator,
     // @ts-ignore
-    async (req: Request, res: Response)=>{
-        const errors = validationResult(req);
-        if(!errors.isEmpty())
-        {
-            return res.status(400).json({errors: errors.array()});
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
         }
-    }
+    },
 )
