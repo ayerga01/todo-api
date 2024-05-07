7071cb6ec1a77bc53cdaf3aecc10024602ef3072
Alba Yerga Castells <ayerga@altech.es>
2024-05-07 13:28:29 +0200

    chore(tasks.validator): add validator in task post

--- src/tasks/tasks.router.ts
+++ src/tasks/tasks.router.ts
@@ -1,13 +1,31 @@
 import { Router, Response, Request } from "express";
 import { TasksController } from "./tasks.controller";
+import { createValidator } from "./tasks.validator";
+import { validationResult } from "express-validator";
 
 // Fire the router function
 export const tasksRouter: Router = Router();
 
 // Create default route
-tasksRouter.get('/tasks', async (req: Request, res: Response)=>{
-    // res.send('Express + TypeScript Server');
-    const tasksController = new TasksController();
-    const allTasks = await tasksController.getAll();
-    res.json(allTasks).status(200);
-})
+tasksRouter.get(
+    '/tasks',
+    async (req: Request, res: Response)=>{
+        // res.send('Express + TypeScript Server');
+        const tasksController = new TasksController();
+        const allTasks = await tasksController.getAll();
+        res.json(allTasks).status(200);
+    }
+)
+
+tasksRouter.post(
+    '/tasks',
+    createValidator,
+    // @ts-ignore
+    async (req: Request, res: Response)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty())
+        {
+            return res.status(400).json({errors: errors.array()});
+        }
+    }
+)
