bc2e9f8451acdcec4bf3fda606436cd6dd909cd7
Alba Yerga Castells <ayerga@altech.es>
2024-05-07 12:08:40 +0200

    chore(tasks.entity): finish task entity structure

--- src/tasks/tasks.entity.ts
+++ src/tasks/tasks.entity.ts
@@ -1,10 +1,30 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
-// import { Priority } from "../enums/Priority";
-// import { Status } from "../enums/Status";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Priority } from "../enums/Priority";
+import { Status } from "../enums/Status";
 
 @Entity() // decorator 
 export class Task {
 
     @PrimaryGeneratedColumn("uuid")
     id: string;
+
+    @Column({ type: "text" })
+    title: string;
+
+    @Column({ type: "longtext"})
+    description: string;
+
+    @Column({
+        type: "enum",
+        enum: Priority,
+        default: Priority.normal,
+    }) 
+    priority: Priority;
+
+    @Column({
+        type: "enum",
+        enum: Status,
+        default: Status.todo,
+    }) 
+    status: Status;
 }
